26a55ef49c088ad68015dd2bbd3a8ddff19b27de
kevin5348 <kevinchristie629@gmail.com>
2025-06-15 14:10:42 +0100

    fixing error with white spaces and adding comments

--- model/cleaningData.py
+++ model/cleaningData.py
@@ -1,17 +1,22 @@
 import math
 import pandas as pd
-with open("/home/kevin/repos/word_reader/model/datasets/freqency/frequency1", "r") as f:
+#opens dataset and gets length of set
+with open("datasets/frequency/frequency1", "r") as f:
     lines = f.readlines()
+# splits data into frequency word and int value plus gets rid of white spaces
+word_counts = {}
+for line in lines:
+    parts = line.strip().split()
+    if len(parts) != 2:
+        continue
+    word, count = parts
+    try:
+        word_counts[word] = int(count)
+    except ValueError:
+        continue
 
-    word_counts = {}
-    for line in lines :
-        word, count = line.split()
-        word_counts[word] = int (count)
+#turns data into a dataFrame and logs the data.Also turns the data into a csv file
+df = pd.DataFrame(list(word_counts.items()), columns=["word", "count"])
+df["log_count"] = df["count"].apply(math.log)
+df.to_csv("word_frequencies.csv", index=False)
 
-        limited_word_counts = dict(list(word_counts.items())[:1000])
-        df = pd.DataFrame(list(limited_word_counts.items()), columns=["word", "count"])
-      
-
-    
-    for i in range(10):
-        print(lines[i])
