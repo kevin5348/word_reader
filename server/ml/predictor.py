f12727a29070f42c1bbbfcd88647ee08291c37b2
kevin5348 <kevinchristie629@gmail.com>
2025-06-22 20:11:53 +0100

    full restructuring into sever,client

---
+++ server/ml/predictor.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import joblib
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+def load_model(model_path="/home/kevin/repos/word_reader/models/difficulty_model.pkl", scaler_path="/home/kevin/repos/word_reader/models/scaler.pkl"):
+    model = joblib.load(model_path)
+    scaler = joblib.load(scaler_path)
+    return model, scaler
+
+def predict_difficulty(df, model, scaler):
+    df["log_count"] = np.log1p(df["count"])
+    X = df[["log_count", "syllables","is_homophone","pronunciation_count"]]
+    df["predicted_difficulty"] = model.predict(scaler.transform(X))
+    return df
