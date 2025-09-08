00e58ffd8436503e543890b5eaa1eef11b7f9297
kevin5348 <kevinchristie629@gmail.com>
2025-09-08 23:28:14 +0100

    adding session logging and click log with auto delete

--- server/database/init_db.py
+++ server/database/init_db.py
@@ -19,7 +19,7 @@ class ClickLog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     word = db.Column(db.Text, nullable=False)
     difficulty_score = db.Column(db.Float, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime)
 
 class WordDifficulty(db.Model):
     __tablename__ = 'word_difficulties'
@@ -32,3 +32,34 @@ class WordDifficulty(db.Model):
     is_homophone = db.Column(db.Boolean, nullable=False)
     pronunciation_count = db.Column(db.BigInteger, nullable=False)
     difficulty_score = db.Column(Numeric(5, 2), nullable=False) 
+
+class Clicked(db.Model):
+    __tablename__ = 'Clicked'
+    id = db.column(db.Integer, primary_key = True)
+    Session_id = db.Column(db.String,db.ForeignKey('session.id', ondelete="CASCADE"),nullable= False)
+    #add word id link to word diff and get diff for word link to the id
+    word_id = db.column(db.Text, nullable = False)
+    clicked = db.column(db.boolean, nullable= False)
+
+
+class Session(db.Model):
+    __tablename__ = 'Sessions'
+    id = db.column(db.Integer, primary_key= True)
+    user_id = db.column(db.Integer,db.Foreign_key('users.id'), nullable = False)
+    session_start = db.column(db.DateTime)
+    session_end = db.column(db.DateTime)
+    total_words = db.column(db.Integer, default = 0)
+    total_clicks = db.column(db.Integer, default = 0)
+    mean_difficulty = db.Column(db.Numeric(5,2))
+
+    clicks = db.relationship(
+        "Clicked",
+        back_populates="session",
+        cascade = "all, delete-orphan"
+        passive_deletes=True
+    )
+
+
+
+
+
--- server/routes/get_clicks.py
+++ server/routes/get_clicks.py
@@ -1,8 +1,15 @@
 from flask import Blueprint ,request ,jsonify
 from auth.middleware import token_required
+from typing import Dict,List
+from database.init_db import ClickLog
 get_clicks_bp = Blueprint("get_clicks", __name__)
 
 @token_required
-def storeClicks():
+@get_clicks_bp.route('/get_clicks', methods=['GET'])
+def storeClicks(wordParam: List[str]):
+
     
 
+
+
+def storeNotClicked(words):
